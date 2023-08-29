103286e5dfa79cd1166f0bfc38ff29a6e9b2211b
ChoByungHyun <cbj973@gmail.com>
2023-08-30 01:26:54 +0900

    ✨ Feat: 무한스크롤 구현

--- src/api/GetIssues.tsx
+++ src/api/GetIssues.tsx
@@ -1,48 +1,63 @@
 import IssueItems from "components/IssueItems";
+import { REQUEST_INFO, octokit } from "config";
+import { useDebounce } from "hooks/useDebounce";
 import { Octokit } from "octokit";
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Issue } from "type";
-const PersonalAccessToken = process.env.REACT_APP_GITHUB_TOKEN;
-const OwnerName = "facebook";
-const RepoName = "react";
-
-const octokit = new Octokit({
-  auth: PersonalAccessToken,
-});
 
 function GetIssue() {
   const [issues, setIssues] = useState<Issue[]>([]);
   const [pageNumber, setPageNumber] = useState<number>(1);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const fetchIssues = async (page: number) => {
+    setLoading(true);
+    try {
+      const response = await octokit.request(
+        `GET /repos/${REQUEST_INFO.OwnerName}/${REQUEST_INFO.RepoName}/issues?page=${page}&sort=comments`,
+        {
+          headers: {
+            "X-GitHub-Api-Version": "2022-11-28",
+          },
+        }
+      );
+      const fetchedIssues: Issue[] = response.data.map((issue: Issue) => ({
+        number: issue.number,
+        title: issue.title,
+        user: issue.user,
+        updated_at: issue.updated_at,
+        comments: issue.comments,
+      }));
+      setIssues((prevIssues) => [...prevIssues, ...fetchedIssues]);
+    } catch (error) {
+      console.error("Error fetching issues:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleScroll = () => {
+    if (
+      !loading &&
+      window.innerHeight + window.scrollY >= document.body.scrollHeight - 500
+    ) {
+      setPageNumber((prevPageNumber) => prevPageNumber + 1);
+    }
+  };
+
+  const debouncedHandleScroll = useDebounce(handleScroll, 300);
 
   useEffect(() => {
-    const fetchIssues = async () => {
-      try {
-        const response = await octokit.request(
-          `GET /repos/${OwnerName}/${RepoName}/issues?page=${pageNumber}&sort=comments`,
-          {
-            headers: {
-              "X-GitHub-Api-Version": "2022-11-28",
-            },
-          }
-        );
-        const fetchedIssues: Issue[] = response.data.map((issue: Issue) => ({
-          number: issue.number,
-          title: issue.title,
-          user: issue.user,
-          updated_at: issue.updated_at,
-          comments: issue.comments,
-        }));
-        console.log(response.data);
-
-        setIssues(fetchedIssues);
-      } catch (error) {
-        console.error("Error fetching issues:", error);
-      }
+    window.addEventListener("scroll", debouncedHandleScroll);
+    return () => {
+      window.removeEventListener("scroll", debouncedHandleScroll);
     };
+  }, [loading, pageNumber]);
 
-    fetchIssues();
-  }, []);
+  useEffect(() => {
+    fetchIssues(pageNumber);
+  }, [pageNumber]);
 
   return (
     <SIssueLayout>
